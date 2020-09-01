e18caf735613495917be248826534d21d8cc983f
Mason Barnes <hypnosismcb@yahoo.com>
2020-09-01 10:13:33 -0700

    Completed get word count exercise

---
+++ get-word-count/main.js
@@ -0,0 +1,8 @@
+function getWordCount(string) {
+  var result = string.split(" ");
+  var length = result.length;
+  console.log(length);
+}
+getWordCount("This is a sample sentence");
+getWordCount("Han shot first");
+getWordCount("I will not fear, fear is the mind killer");
