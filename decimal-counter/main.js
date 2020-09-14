1f661560d52ac127aaa7ba979f83197475a0e1b4
Mason Barnes <hypnosismcb@yahoo.com>
2020-09-14 16:33:27 -0700

    Completed decimal counter exercise

--- decimal-counter/main.js
+++ decimal-counter/main.js
@@ -1,10 +1,19 @@
 function decimalPlaceCounter(stringNumber) {
-  var newNum = parseFloat(stringNumber);
-  console.log(newNum);
+  var blank = 0;
+  var decimalFound = false;
+  for (var i=0; i < stringNumber.length; i++) {
+    if (stringNumber[i] === '.') {
+      decimalFound = true;
+    } else if (decimalFound) {
+      blank++;
+    }
+  }
+  return blank;
 }
 
 
 
-decimalPlaceCounter("127.245");
-decimalPlaceCounter("145");
-decimalPlaceCounter("23.00");
+
+console.log("Numbers after decimal: ", decimalPlaceCounter("127.245"));
+console.log("Numbers after decimal: ", decimalPlaceCounter("145"));
+console.log("Numbers after decimal: ", decimalPlaceCounter("23.00"));
