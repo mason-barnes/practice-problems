5addad5c9b28298ea73fdf8030eb35b7a3b51f95
Mason Barnes <hypnosismcb@yahoo.com>
2020-09-15 10:21:08 -0700

    cleaned up some old habits on code

--- odd-up-even-down/main.js
+++ odd-up-even-down/main.js
@@ -7,9 +7,9 @@ function oddUpEvenDown(array) {
       newArray.push(array[i] - 1);
     }
   }
-  console.log(newArray);
+  return newArray;
 }
 
-oddUpEvenDown([1, 2, 3, 4, 5]);
-oddUpEvenDown([23, 15, 18, 14, 32]);
-oddUpEvenDown([72, 212, 77, 124, 53]);
+console.log(oddUpEvenDown([1, 2, 3, 4, 5]));
+console.log(oddUpEvenDown([23, 15, 18, 14, 32]));
+console.log(oddUpEvenDown([72, 212, 77, 124, 53]));
